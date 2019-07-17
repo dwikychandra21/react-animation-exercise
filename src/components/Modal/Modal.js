9bbfb136a84430764e69a96cfb3cea1e68758d0c
dwikychandra21 <dwikychandra21@gmail.com>
2019-07-17 21:59:28 +0800

    use css transition and transition group

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,38 +1,28 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./Modal.css";
 
 const modal = props => {
-  const cssClasses = [
-    "Modal",
-    props.show ? "ModalOpen" : "ModalClosed"
-  ];
-
   const animationTimeout = {
-      enter: 400,
+      enter: 2000,
       exit: 1000
   }
 
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTimeout}
       unmountOnExit
-      onEnter={() => console.log('onEnter')}
-      onEntering={() => console.log('onEntering')}
-      onEntered={() => console.log('onEntered')}
-      onExit={() => console.log('onExit')}
-      onExiting={() => console.log('onExiting')}
-      onExited={() => console.log('onExited')}
+      classNames="fade-slide"
     >
-      <div className={cssClasses.join(" ")}>
+      <div className="Modal">
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>
           Dismiss
         </button>
       </div>
-    </Transition>
+    </CSSTransition>
   );
 };
 
